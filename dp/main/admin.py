0d53886acf0d71df65c0d8590c42eaf73e8c0bd1
Георги Бориков <pgee_dzi2024@abv.bg>
2025-03-30 17:48:59 +0300

    Визия ОК

--- dp/main/admin.py
+++ dp/main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Poll)
+class PollV(admin.ModelAdmin):
+    list_display = ('id', 'theme', 'description', )
+    list_display_links = ('theme', 'description',)
+    ordering = ('-id', )
+
+@admin.register(Question)
+class QuestionV(admin.ModelAdmin):
+    list_display = ('id', 'text', )
+    list_display_links = ('id', 'text',)
+    list_filter = ('pool_id', )
+    ordering = ('-id', )
+
+@admin.register(Option)
+class OptionV(admin.ModelAdmin):
+    list_display = ('id', 'text', )
+    list_display_links = ('id', 'text',)
+    list_filter = ('question_id', )
+    ordering = ('-id', )
+
--- dp/main/views.py
+++ dp/main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    # заявка за всички активни анкети
+    polls = Poll.objects.filter(active=True)
+
+    context = {'polls': polls}
+    return render(request, 'main/index.html', context)
+
+
+def poll(request, poll_id):
+    # заявка за всички анкети със всички въпроси за всяка анкета със всички отговори за всеки въпрос
+    polls = Poll.objects.prefetch_related('question_set__options').filter(id=poll_id)
+
+    context = {'polls': polls}
+    return render(request, 'main/poll.html', context)
